d51138f5ef477cf137d89bfff20c008c8c847cca
OssianN <ossian.northen@gmail.com>
2022-02-01 14:18:22 +0100

    divides buzzed and not buzzed into two lists

--- components/VipRoom.js
+++ components/VipRoom.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import styles from "../styles/Home.module.css";
+import styles from "./vipRoom.module.css";
 
 const Vip = ({ handleResetBuzz }) => {
   const { usersList } = useSelector((state) => state.userSlice);
+  const buzzed = usersList.filter((user) => user.buzzed);
+  const notBuzzed = usersList.filter((user) => !user.buzzed);
 
   return (
-    <div className={styles.main}>
-      <button onClick={handleResetBuzz}>RESET</button>
-      <ul>
-        {[...usersList]
-          ?.sort((a, b) => a.buzzed - b.buzzed)
+    <div className={styles.host__container}>
+      <ul className={styles.users__ul}>
+        {buzzed
+          .sort((a, b) => a.buzzed - b.buzzed)
           .map((user, i) => {
             return (
-              <li key={i}>
+              <li className={styles.users__li} key={i}>
                 <h2>{user.name}</h2>
                 <p>{user.buzzed}</p>
               </li>
             );
           })}
+        <button onClick={handleResetBuzz}>RESET</button>
+      </ul>
+
+      <ul className={styles.users__ul}>
+        {notBuzzed.map((user, i) => {
+          return (
+            <li className={styles.users__li} key={i}>
+              <h2>{user.name}</h2>
+              <p>{user.buzzed}</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
